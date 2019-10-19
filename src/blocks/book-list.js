dfb056f0b5087428fe196a443c7cfcefe4968e8c
Doxxy1 <s3602535@student.rmit.edu.au>
2019-10-19 19:41:36 +1100

    Update book-list.js

--- src/blocks/book-list.js
+++ src/blocks/book-list.js
@@ -5,31 +5,47 @@ import Image from "components/grid/image";
 import Details from "components/grid/details";
 import Paper from "@material-ui/core/Paper";
 import { AuthContext } from "../auth.js";
+import * as firebase from 'firebase';
 
 import ImageSrc from "assets/mitch.jpg";
 
+/*const data = [
+    [ImageSrc, "Book1", "Author1", "$12.99"],
+    [ImageSrc, "Book2", "Author2", "$15.99"],
+    [ImageSrc, "Book3", "Author3", "$22.99"],
+    [ImageSrc, "Book4", "Author4", "$33.99"],
+    [ImageSrc, "Book5", "Author5", "$17.99"],
+    [ImageSrc, "Book6", "Author6", "$56.99"]
+];
+*/
+
+
+const data = [];
+
+
+
+
 const BookList = () => {
-    const data = [];
-let citiesRef = firebase.firestore().collection('books');
-let allCities = citiesRef.get()
-  .then(snapshot => {
-    snapshot.forEach(doc => {
-      console.log(doc.id, '=>', doc.data());
-      data.push(doc.data());
-      console.log(data);
-    });
-  })
-    const books = data.map(book => (    
+    let citiesRef = firebase.firestore().collection('books');
+    let allCities = citiesRef.get()
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                data.push(doc.data());
+            });
+        })
+
+
+    const books = data.map(book => (
         <Grid container direction="column" xs={6} md={4}>
-            <Image src={book[1]} />
+            <Image src={book[0]} />
             <Details title={book[1]} author={book[2]} price={book[3]} />
         </Grid>
     ))
     const { currentUser } = useContext(AuthContext);
     console.log(currentUser);
-    
+    console.log(data);
     return (
-        
+
         <Paper
             style={{
                 padding: "16px",
@@ -39,8 +55,10 @@ let allCities = citiesRef.get()
         >
             <Grid container spacing={3}>
                 {books}
+
             </Grid>
         </Paper>
+
     );
 };
 export default BookList;
